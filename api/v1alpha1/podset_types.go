42c5e418cebb698ccec5e1c73a0361a20f6ce6f8
Austin Macdonald <austin@redhat.com>
2022-10-17 14:03:29 -0400

    modify podset_types

--- api/v1alpha1/podset_types.go
+++ api/v1alpha1/podset_types.go
@@ -28,14 +28,17 @@ type PodSetSpec struct {
 	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
 
-	// Foo is an example field of PodSet. Edit podset_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+	// +kubebuilder:validation:Minimum=1
+	// +kubebuilder:validation:Maximum=10
+	Replicas int32 `json:"replicas,omitempty"`
 }
 
 // PodSetStatus defines the observed state of PodSet
 type PodSetStatus struct {
 	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
 	// Important: Run "make" to regenerate code after modifying this file
+	PodNames          []string `json:"podNames"`
+	AvailableReplicas int32    `json:"availableReplicas"`
 }
 
 //+kubebuilder:object:root=true
